d6d25764c0e624327207cbd390de93e952f4d5be
lencedev <owen1.bolling@epitech.eu>
2024-09-26 09:45:15 +0200

    update

--- src/Components/ResumeButton.js
+++ src/Components/ResumeButton.js
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Button, Modal } from 'react-bootstrap';
-import ResumeIMG from "../assets/img/cv2.jpg";
+import ResumeIMG from "../assets/img/cv_owen.jpg";
 
 export const ResumeButton = ({ className }) => {
     const [show, setShow] = useState(false);
